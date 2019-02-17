37556429404eba9872e41f5a77f19cbb4f583954
limkeunhyeok <gorloom6425@naver.com>
2019-02-17 21:48:18 +0900

    Stack

--- study.py
+++ study.py
@@ -11,10 +11,15 @@ class Node:
     def SetNext(self,next):
         self.next = next
 
-class LinkedList:
+class Stack:
     def __init__(self):
         self.top = None
-    def Add(self,data):
+    def is_empty(self):
+        if self.top == None:
+            return True
+        else:
+            return False
+    def Push(self, data):
         new_node = Node(data)
         if self.top == None:
             self.top = new_node
@@ -23,64 +28,18 @@ class LinkedList:
             while current.GetNext() != None:
                 current = current.GetNext()
             current.SetNext(new_node)
-    def Remove(self):
-        if self.top == None:
-            print("empty")
-        else:
-            current = self.top
-            previous = current
-            while current.GetNext() != None:
-                previous = current
-                current = current.GetNext()
-            previous.SetNext(None)
-            return current.GetData()
-    def Count(self):
-        count = 1
+    def Pop(self):
         current = self.top
-        if self.top == None:
-            return 0
+        if current.GetNext() == None:
+            self.__init__()
+            return current.GetData()
+        while current.GetNext() != None:
+            previous = current
+            current = current.GetNext()
+        previous.SetNext(None)
+        return current.GetData()
+    def Peek(self):
+        current = self.top
         while current.GetNext() != None:
             current = current.GetNext()
-            count += 1
-        return count
-    def Clear(self):
-        self.__init__()
-    def Contains(self,data):
-        current = self.top
-        count = 0
-        for i in range(self.Count()):
-            if current.GetData() == data:
-                print('%d는 %d번째에 있습니다.' % (data, count))
-            current = current.GetNext()
-            count += 1
-    def Sort(self):
-        current = self.top
-        previous = current
-        for i in range(self.Count() - 1):
-            for j in range(self.Count() - 1):
-                previous = current
-                current = current.GetNext()
-                if current.GetData() < previous.GetData():
-                    value1 = current.GetData()
-                    value2 = previous.GetData()
-                    current.SetData(value2)
-                    previous.SetData(value1)
-            current = self.top
-    def PrintList(self):
-        if self.top != None:
-            current = self.top
-            print(current.GetData(),end = ' ')
-            while current.GetNext() != None:
-                current = current.GetNext()
-                if current.GetNext() == None:
-                    print(current.GetData())
-                else:
-                    print(current.GetData(), end = ' ')
-
-
-list = LinkedList()
-print(list.Count())
-list.PrintList()
-list.Sort()
-list.PrintList()
-print(list.Count())
+        return current.GetData()
