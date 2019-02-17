619bf38d61bbfb347e2476b9bc55edca72cf35d5
limkeunhyeok <gorloom6425@naver.com>
2019-02-17 22:14:54 +0900

    Queue

--- study.py
+++ study.py
@@ -2,44 +2,31 @@ class Node:
     def __init__(self,data):
         self.data = data
         self.next = None
-    def GetData(self):
-        return self.data
-    def SetData(self,data):
-        self.data = data
-    def GetNext(self):
-        return self.next
-    def SetNext(self,next):
-        self.next = next
 
-class Stack:
+class Queue:
     def __init__(self):
-        self.top = None
+        self.head = None
+        self.tail = None
     def is_empty(self):
-        if self.top == None:
+        if self.head == None:
             return True
         else:
             return False
-    def Push(self, data):
+    def enqueue(self, data):
         new_node = Node(data)
-        if self.top == None:
-            self.top = new_node
+        if self.is_empty():
+            self.head = new_node
+            self.tail = new_node
         else:
-            current = self.top
-            while current.GetNext() != None:
-                current = current.GetNext()
-            current.SetNext(new_node)
-    def Pop(self):
-        current = self.top
-        if current.GetNext() == None:
-            self.__init__()
-            return current.GetData()
-        while current.GetNext() != None:
-            previous = current
-            current = current.GetNext()
-        previous.SetNext(None)
-        return current.GetData()
-    def Peek(self):
-        current = self.top
-        while current.GetNext() != None:
-            current = current.GetNext()
-        return current.GetData()
+            self.tail.next = new_node
+            self.tail = new_node
+    def dequeue(self):
+        if self.is_empty():
+            return False
+        value = self.head.data
+        self.head = self.head.next
+        return value
+    def peek(self):
+        if self.is_empty():
+            return False
+        return self.head.data
